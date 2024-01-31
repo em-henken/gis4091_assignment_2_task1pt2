28b7a327f5945e4eb9a9a1a736e7066b93e7b9cc
em-henken <118228404+em-henken@users.noreply.github.com>
2024-01-31 12:37:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,24 +14,10 @@
           zoom: 11
         });
 
-        /********************
-         * Add feature layer
-         ********************/
 
-        // Carbon storage of trees in Warren Wilson College.
-   /*
-        var featureLayer = new FeatureLayer({
-           portalItem: {
-            id: "bbdd49e10a2847a8b8018a7035bc92c7"    
-          }
-        });
-        map.add(featureLayer);
-   
-   */
-var featureLayer1 = new FeatureLayer({  url:"https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/Depression_Prevalence/FeatureServer"
-})
-var featureLayer2 = new FeatureLayer({    url:"https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/STL_City_Crime01_WFL1/FeatureServer"
-})
+        var featureLayer1 = new FeatureLayer({url:"https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/Depression_Prevalence/FeatureServer"})
+        var featureLayer2 = new FeatureLayer({url:"https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/STL_City_Crime01_WFL1/FeatureServer"})
+  
         map.add(featureLayer1);
         map.add(featureLayer2);
 });
